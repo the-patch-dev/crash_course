e27b58cd44575f0a74d40eb8119097e7ed764abb
Patrick <patrickmarkhambrown@gmail.com>
2024-10-15 19:34:24 +1100

    testing nvim plugins

---
+++ chapter_1/hello_world.py
@@ -0,0 +1,15 @@
+# first file created for crash course
+# \n creates new lin
+var0 = 2
+var1 = 4
+print("\nHello, world!\n")
+chloe_response = input("\nHow are you?\n")
+# lower() function makes the comparison case-insensitive
+if chloe_response.lower() == "good" or "greatgreat":
+    print("nice!" + str(var0))
+else:
+    print("not nice")
+print("this is a test ffs")
+# this is another line to test the end of the script
+# this is me testing some code
+print("hello world")
